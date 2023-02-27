59a0b9dda7599b52d6c1e14adbd838da55e57419
Fernando Herrera <fernando.herrera85@gmail.com>
2023-02-27 17:42:35 -0500

    No hay favoritos

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -2,6 +2,7 @@ import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:go_router/go_router.dart';
 
 
 class FavoritesView extends ConsumerStatefulWidget {
@@ -43,6 +44,30 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   Widget build(BuildContext context) {
 
     final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
+
+    if ( favoriteMovies.isEmpty ) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon( Icons.favorite_outline_sharp, size: 60, color: colors.primary ),
+            Text('Ohhh no!!', style: TextStyle( fontSize: 30, color: colors.primary)),
+            const Text('No tienes películas favoritas', style: TextStyle( fontSize: 20, color: Colors.black45 )),
+
+            const SizedBox(height: 20),
+            FilledButton.tonal(
+              onPressed: () => context.go('/home/0'), 
+              child: const Text('Empieza a buscar')
+            )
+          ],
+        ),
+      );
+    }
+
+
 
 
     return Scaffold(
