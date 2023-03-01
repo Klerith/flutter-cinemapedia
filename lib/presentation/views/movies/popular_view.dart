91ebcd555f28a00aa811318fc5af190f9b68eac8
Fernando Herrera <fernando.herrera85@gmail.com>
2023-03-01 14:02:58 -0500

    popular movies added

--- lib/presentation/views/movies/popular_view.dart
+++ lib/presentation/views/movies/popular_view.dart
@@ -1,16 +1,24 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PopularView extends StatelessWidget {
+class PopularView extends ConsumerWidget {
   const PopularView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref ) {
+    
+    final popularMovies = ref.watch( popularMoviesProvider );
+    
+    if ( popularMovies.isEmpty ) {
+      return const Center(child: CircularProgressIndicator(strokeWidth: 2));
+    }
     
     return Scaffold(
       body: MovieMasonry(
-        // loadNextPage: loadNextPage,
-        movies: []
+        loadNextPage: () => ref.read(popularMoviesProvider.notifier).loadNextPage(),
+        movies: popularMovies
       )
     );
   }
