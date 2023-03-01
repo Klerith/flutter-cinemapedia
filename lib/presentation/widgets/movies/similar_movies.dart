7055c287746da1e6dd75da1f95761148e7904fd0
Fernando Herrera <fernando.herrera85@gmail.com>
2023-03-01 14:16:46 -0500

    similar movies empty

--- lib/presentation/widgets/movies/similar_movies.dart
+++ lib/presentation/widgets/movies/similar_movies.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:cinemapedia/domain/entities/entities.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 
@@ -21,15 +22,29 @@ class SimilarMovies extends ConsumerWidget {
     final similarMoviesFuture = ref.watch(similarMoviesProvider(movieId));
 
     return similarMoviesFuture.when(
-      data: ( movies) => Container(
-        margin: const EdgeInsetsDirectional.only(bottom: 50),
-        child: MovieHorizontalListview(
-          title: 'Recomendaciones',
-          movies: movies
-        ),
-      ), 
+      data: ( movies) => _Recomendations( movies: movies ), 
       error: (_ , __) => const Center(child: Text('No se pudo cargar películas similares') ), 
       loading: () => const Center(child: CircularProgressIndicator(strokeWidth: 2)),
+    );
+  }
+}
+
+class _Recomendations extends StatelessWidget {
+  final List<Movie> movies;
+
+  const _Recomendations({ required this.movies });
+
+  @override
+  Widget build(BuildContext context) {
+
+    if ( movies.isEmpty ) return const SizedBox();
+
+    return Container(
+      margin: const EdgeInsetsDirectional.only(bottom: 50),
+      child: MovieHorizontalListview(
+        title: 'Recomendaciones',
+        movies: movies
+      ),
     );
   }
 }
