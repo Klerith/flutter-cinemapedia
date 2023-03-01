2d9556375ff92d4e427ed62a8f78511b0a66c3fa
Fernando Herrera <fernando.herrera85@gmail.com>
2023-03-01 12:23:32 -0500

    video entity added

---
+++ lib/domain/entities/video.dart
@@ -0,0 +1,16 @@
+
+
+class Video {
+
+  final String id;
+  final String name;
+  final DateTime publishedAt;
+
+  Video({
+    required this.id,
+    required this.name,
+    required this.publishedAt
+  });
+
+
+}
