9c5decd6049820383be2ec81c63aa0a173fe5f0b
Fernando Herrera <fernando.herrera85@gmail.com>
2023-03-01 14:47:39 -0500

    search delegate fadeIn image

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -8,7 +8,7 @@ class AppTheme {
 
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
-    colorSchemeSeed: const Color(0xFF2862F5),
+    colorSchemeSeed: Colors.white,
     brightness: Brightness.dark
   );
 
--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -164,54 +164,58 @@ class _MovieItem extends StatelessWidget {
       onTap: () {
         onMovieSelected(context, movie);
       },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        child: Row(
-          children: [
-    
-            // Image
-            SizedBox(
-              width: size.width * 0.2,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.network( 
-                  movie.posterPath,
-                  loadingBuilder: (context, child, loadingProgress) => FadeIn(child: child),
+      child: FadeIn(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          child: Row(
+            children: [
+          
+              // Image
+              SizedBox(
+                width: size.width * 0.2,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: FadeInImage(
+                    height: 130,
+                    fit: BoxFit.cover,
+                    image: NetworkImage(movie.posterPath),
+                    placeholder: const AssetImage('assets/loaders/bottle-loader.gif'),
+                  )
                 ),
               ),
-            ),
-    
-            const SizedBox(width: 10),
-            
-            // Description
-            SizedBox(
-              width: size.width * 0.7,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text( movie.title, style: textStyles.titleMedium ),
-    
-                  ( movie.overview.length > 100 )
-                    ? Text( '${movie.overview.substring(0,100)}...' )
-                    : Text( movie.overview ),
-    
-                  Row(
-                    children: [
-                      Icon( Icons.star_half_rounded, color: Colors.yellow.shade800 ),
-                      const SizedBox(width: 5),
-                      Text( 
-                        HumanFormats.number(movie.voteAverage, 1),
-                        style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900 ),
-                      ),
-                    ],
-                  )
-    
-                  
-                ],
+          
+              const SizedBox(width: 10),
+              
+              // Description
+              SizedBox(
+                width: size.width * 0.7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text( movie.title, style: textStyles.titleMedium ),
+          
+                    ( movie.overview.length > 100 )
+                      ? Text( '${movie.overview.substring(0,100)}...' )
+                      : Text( movie.overview ),
+          
+                    Row(
+                      children: [
+                        Icon( Icons.star_half_rounded, color: Colors.yellow.shade800 ),
+                        const SizedBox(width: 5),
+                        Text( 
+                          HumanFormats.number(movie.voteAverage, 1),
+                          style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900 ),
+                        ),
+                      ],
+                    )
+          
+                    
+                  ],
+                ),
               ),
-            ),
-    
-          ],
+          
+            ],
+          ),
         ),
       ),
     );
