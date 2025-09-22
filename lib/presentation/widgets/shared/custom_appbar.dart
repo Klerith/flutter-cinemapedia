1046174f0da133db87d327f361a74521da9b3bfd
Fernando Herrera <fernando.herrera85@gmail.com>
2025-09-22 10:24:29 -0400

    Update dependencies and iOS deployment target; enhance .gitignore and refactor MovieDbResponse model

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -12,49 +12,44 @@ class CustomAppbar extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
 
     return SafeArea(
-      bottom: false,
-      child: Padding(
-        padding: const EdgeInsets.symmetric( horizontal: 10 ),
-        child: SizedBox(
-          width: double.infinity,
-          child: Row(
-            children: [
-              Icon( Icons.movie_outlined, color: colors.primary ),
-              const SizedBox( width: 5 ),
-              Text('Cinemapedia', style: titleStyle ),
-      
-              const Spacer(),
-      
-              IconButton(onPressed: () {
+        bottom: false,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: SizedBox(
+            width: double.infinity,
+            child: Row(
+              children: [
+                Icon(Icons.movie_outlined, color: colors.primary),
+                const SizedBox(width: 5),
+                Text('Cinemapedia', style: titleStyle),
+                const Spacer(),
+                IconButton(
+                    onPressed: () {
+                      final searchedMovies = ref.read(searchedMoviesProvider);
+                      final searchQuery = ref.read(searchQueryProvider);
 
-                final searchedMovies = ref.read( searchedMoviesProvider );
-                final searchQuery = ref.read(searchQueryProvider);
-                
-                showSearch<Movie?>(
-                  query: searchQuery,
-                  context: context, 
-                  delegate: SearchMovieDelegate(
-                    initialMovies: searchedMovies,
-                    searchMovies: ref.read( searchedMoviesProvider.notifier ).searchMoviesByQuery
-                  )
-                ).then((movie) {
-                  if ( movie == null ) return;
+                      showSearch<Movie?>(
+                              query: searchQuery,
+                              context: context,
+                              delegate: SearchMovieDelegate(
+                                  initialMovies: searchedMovies,
+                                  searchMovies: ref
+                                      .read(searchedMoviesProvider.notifier)
+                                      .searchMoviesByQuery))
+                          .then((movie) {
+                        if (movie == null || !context.mounted) return;
 
-                  context.push('/movie/${ movie.id }');
-                });
-
-              }, 
-              icon: const Icon(Icons.search)
-              )
-            ],
+                        context.push('/movie/${movie.id}');
+                      });
+                    },
+                    icon: const Icon(Icons.search))
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
